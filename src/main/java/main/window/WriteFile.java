5cda2a6b96e394469918ea965d2f2caa572f52d2
janyczek <jasiek7>
2021-02-10 21:02:00 +0100

    użycie bazy danych tylko do odczytu

--- src/main/java/main/window/WriteFile.java
+++ src/main/java/main/window/WriteFile.java
@@ -4,10 +4,13 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
 
 public class WriteFile {
 
-    public void write(String text){
+    public void write(String text) {
         final String file="C:\\Users\\Jan\\Desktop\\ThingsToDo.txt";
 
         try(
@@ -22,6 +25,21 @@ public class WriteFile {
             System.err.println("Something went bad");
             e.printStackTrace();
         }
+        try (
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/todoapp?serverTimezone=UTC", "root", "jasiek7");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT number, priority, name FROM main_table LIMIT 3");) {
 
+            while (resultSet.next()) {
+                int firstName = resultSet.getInt("number");
+                int lastName = resultSet.getInt("priority");
+                String hireDate = resultSet.getString("name");
+                System.out.printf("%d %s - data zatrudnienia: %s\n", firstName, lastName, hireDate);
+
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,9 @@ module FirstApp {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires mysql.connector.java;
+    requires com.google.protobuf;
+    requires java.sql;
 
     opens controllers to javafx.fxml;
 
